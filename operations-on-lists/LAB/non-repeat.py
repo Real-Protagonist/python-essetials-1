6639cfde885c99ae8b100b6dab2979b4160b3d52
Athena <athena@localhost.localdomain>
2024-12-02 12:07:01 +0100

    LAB done.

---
+++ operations-on-lists/LAB/non-repeat.py
@@ -0,0 +1,10 @@
+my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+temp_list = []
+
+for i in my_list:
+    if i not in temp_list:
+        temp_list.append(i)
+
+my_list = temp_list[:]
+del temp_list
+print(my_list)
