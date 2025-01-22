483476e63724725cb4f66f7e4f2ddb7763b2d66e
Athena <athena@localhost.localdomain>
2025-01-22 09:38:40 +0100

    Add a new key in the dic.

--- dictionaries/dic-and-functions/dic-03.py
+++ dictionaries/dic-and-functions/dic-03.py
@@ -14,3 +14,15 @@ print()
 
 for french in dictionary.values():
     print(french)
+
+# ADDING A NEW KEY
+print()
+print("ADDING A NEW KEY")
+dictionary['swan'] = 'cygne'
+print(dictionary)
+
+# ADDING A NEW KEY BY UPDATE THE DIC
+print()
+print("ADDING A NEW KEY BY UPDATE")
+dictionary.update({"duck": "canard"})
+print(dictionary)
